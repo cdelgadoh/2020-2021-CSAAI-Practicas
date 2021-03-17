a55e9ca946f363f623f561c1ce216b2f1baeb7e0
cdelgadoh <crisdelgadohuertas@gmail.com>
2021-03-17 13:25:01 +0100

    Ejercicio 2

---
+++ P1/S4/Ejercicio2.js
@@ -0,0 +1,12 @@
+console.log("Ejecutnado JS...");
+
+//-- Obtener el botón
+const boton = document.getElementById('boton');
+const test1 = document.getElementById('test1');
+
+
+//-- Funcion de retrollamada de la pulsación del botón
+boton.onclick = () => {
+    console.log("Click!");
+    test1.innerHTML = "Parrafo añadido"
+}
