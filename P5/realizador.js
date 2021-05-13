18ea7affc74f86c2545554c9a620339ca73eee8a
cdelgadoh <crisdelgadohuertas@gmail.com>
2021-05-13 11:33:09 +0200

    Cambios

--- P5/realizador.js
+++ P5/realizador.js
@@ -4,26 +4,31 @@ const video1 = document.getElementById("video1");
 const btn_video1 = document.getElementById("btn_video1");
 const video2 = document.getElementById("video2");
 const btn_video2 = document.getElementById("btn_video2");
+const video3 = document.getElementById("video3");
+const btn_video3 = document.getElementById("btn_video3");
 const btn_test = document.getElementById("btn_test");
 const btn_src_on = document.getElementById("btn_src_on");
 const btn_src_off = document.getElementById("btn_src_off");
 
 //-- Establecer las dimensiones de los vídeos
-directo.width=420;
+directo.width=400;
 directo.height=200;
 video1.width=200;  
 video1.height=100;
 video2.width=200;  
 video2.height=100;
+video3.width=200;  
+video3.height=100;
 
 //-- Imagen de Test usada
-const TEST_IMAGE_URL = "test.png";
+const TEST_IMAGE_URL = "noseñal.jpg";
 
 //-- Imagen estática a mostrar cuando el video no
 //-- ha arrancado
 directo.poster = TEST_IMAGE_URL;
 video1.poster = TEST_IMAGE_URL;
 video2.poster = TEST_IMAGE_URL;
+video3.poster = TEST_IMAGE_URL;
 
 //-- Boton de FUENTES-ON
 btn_src_on.onclick = () => {
@@ -31,16 +36,20 @@ btn_src_on.onclick = () => {
   //-- Establecer la fuente de la cámara 1
   video1.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4";
   video2.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente2.mp4";
+  video3.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente3.mp4";
 
   //-- Reprodeucimos un vídeo, desde el comienzo
   video1.currentTime = 0;
   video1.play();
   video2.currentTime = 0;
   video2.play();
+  video3.currentTime = 0;
+  video3.play();
 
   //-- Y en silencio...
-  video1.muted;
-  video2.muted;
+  video1.muted ="true";
+  video2.muted ="true";
+  video3.muted ="true";
 
   //-- En la emisión en directo ponemos la imagen de prueba
   directo.poster = TEST_IMAGE_URL;
@@ -48,8 +57,6 @@ btn_src_on.onclick = () => {
 
 //-- Boton Fuentes OFF
 btn_src_off.onclick = () => {
-    video1.pause();
-    video2.pause();
     directo.src = null;
     directo.poster = TEST_IMAGE_URL;
 
@@ -68,10 +75,18 @@ btn_video1.onclick = () => {
     directo.poster=null;
 };
 
-//-- Botón de Selección de la cámara 1
+//-- Botón de Selección de la cámara 2
 btn_video2.onclick = () => {
     directo.src = video2.src;
     directo.currentTime = video2.currentTime;
+    directo.play();
+    directo.poster=null;
+};
+
+//-- Botón de Selección de la cámara 3
+btn_video2.onclick = () => {
+    directo.src = video3.src;
+    directo.currentTime = video3.currentTime;
     directo.play();
     directo.poster=null;
 };
